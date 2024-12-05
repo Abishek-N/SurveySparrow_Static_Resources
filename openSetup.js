41b436a912c9c23f8127a9fbf989b27dcb2d68b8
Abishek N <52904525+Abishek-N@users.noreply.github.com>
2024-12-05 17:57:09 +0530

    Add files via upload

---
+++ openSetup.js
@@ -0,0 +1,9 @@
+$Lightning.use("SVSP:SS_Classic_Survey_Sparrow_App", function() {
+        
+    $Lightning.createComponent(
+        "SVSP:SS_SurveyRunnerSetupComp",
+        {ButtonShow: true},
+        "compContainer",
+         function() {}
+     );
+});
---
+++ openSurveyRunner.js
@@ -0,0 +1,9 @@
+$Lightning.use("SVSP:SS_Classic_Survey_Sparrow_App", function() {
+        
+    $Lightning.createComponent(
+        "SVSP:SS_SurveyRunnerListViewComp",
+        {ButtonShow: true},
+        "compContainer",
+         function() {}
+     );
+});
